4f82bc5ec8e7b8e50bafeafd9f6efba38aa72a40
britg <brit@britg.com>
2015-02-15 19:14:49 -0600

    Add websockets library

---
+++ Assets/Scripts/WebsocketsTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using WebSocketSharp;
+
+public class WebsocketsTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartWebsockets();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void StartWebsockets () {
+		using (var ws = new WebSocket ("ws://dragonsnest.far/Laputa")) {
+			Debug.Log ("Starting websockets");
+			ws.OnMessage += (sender, e) =>
+				Debug.Log ("Laputa says: " + e.Data);
+
+			ws.OnOpen += (sender, e) =>
+				Debug.Log ("On open " + e);
+
+			ws.OnError += (sender, e) => 
+				Debug.Log ("On error " + e.Exception);
+			
+			ws.Connect ();
+			ws.Send ("BALUS");
+		}
+	}
+}
